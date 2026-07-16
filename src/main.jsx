7c56de195a67a1d98e4459394319a5e355b72beb
Nyaguthii-C <nyaguthiigc@gmail.com>
2026-07-17 02:52:02 +0300

    Feat: Initial app setup

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import ReactDOM from "react-dom/client";
+import './index.css'
+import '../styles/theme.css';
+import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { AuthProvider } from "../contexts/AuthContext";
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <AuthProvider>
+            <App />
+        </AuthProvider>
+    </React.StrictMode>
+);
