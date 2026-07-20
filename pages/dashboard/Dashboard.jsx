bb27ba1809dbf0b833fac8d7d396c046ab0a766c
Nyaguthii-C <nyaguthiigc@gmail.com>
2026-07-21 00:54:27 +0300

    Feat: add components

--- pages/dashboard/Dashboard.jsx
+++ pages/dashboard/Dashboard.jsx
@@ -1,64 +1,116 @@
-import MainLayout from "../../components/layout/MainLayout";
+// import MainLayout from "../../components/layout/MainLayout";
 
-export default function Dashboard(){
+// export default function Dashboard(){
 
-return(
+// return(
 
-<MainLayout>
+// <MainLayout>
 
-<h2>
+// <h2>
 
-Dashboard
+// Dashboard
 
-</h2>
+// </h2>
 
-</MainLayout>
+// </MainLayout>
 
-)
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // export default function Dashboard() {
-// //     return <h1>Dashboard</h1>;
-// // }
-
-// import { useAuth } from "../../contexts/AuthContext";
-
-// export default function Dashboard() {
-
-//     const { user } = useAuth();
-
-//     return (
-
-//         <div className="container mt-5">
-
-//             <h2>
-
-//                 Welcome {user?.user?.username || user?.username}
-
-//             </h2>
-
-//             <pre>
-
-//                 {JSON.stringify(user, null, 2)}
-
-//             </pre>
-
-//         </div>
-
-//     );
+// )
 
 // }
+
+import { useEffect, useState } from "react";
+
+import TodayExpenses from "../../components/dashboard/TodayExpenses";
+import MainLayout from "../../components/layout/MainLayout";
+import BudgetCard from "../../components/dashboard/BudgetCard";
+import QuickExpense from "../../components/dashboard/QuickExpense";
+
+
+import { getDashboardData } from "../../services/dashboardService";
+
+export default function Dashboard() {
+
+    const greeting = () => {
+
+        const hour = new Date().getHours();
+
+        if (hour < 12) return "Good Morning";
+
+        if (hour < 18) return "Good Afternoon";
+
+        return "Good Evening";
+
+    };    
+
+    const [dashboard, setDashboard] = useState(null);
+
+    const [loading, setLoading] = useState(true);
+
+    async function loadDashboard() {
+
+        try {
+
+            const data = await getDashboardData();
+
+            setDashboard(data);
+
+        } catch (error) {
+
+            console.error(error);
+
+        } finally {
+
+            setLoading(false);
+
+        }
+
+    }
+
+    useEffect(() => {
+
+        loadDashboard();
+
+    }, []);
+
+    if (loading) {
+
+        return (
+
+            <MainLayout>
+
+                <p>Loading dashboard...</p>
+
+            </MainLayout>
+
+        );
+
+    }
+
+    return (
+
+        <MainLayout>
+
+            <h2 className="mb-0">
+
+                {greeting()} 👋
+
+            </h2>
+
+            <BudgetCard
+                budget={dashboard.budget}
+                monthly={dashboard.monthly}
+            />
+
+            <QuickExpense
+                onSuccess={loadDashboard}
+            />
+
+            <TodayExpenses
+                expenses={dashboard.today}
+            />
+
+        </MainLayout>
+
+    );
+
+}
