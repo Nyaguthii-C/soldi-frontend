bc0c2304b4ba4b649bafb9d2489012ce66dccb27
Nyaguthii-C <nyaguthiigc@gmail.com>
2026-07-24 06:51:44 +0300

    Refactor: add layout to navbar items

--- components/navigation/Navbar.jsx
+++ components/navigation/Navbar.jsx
@@ -7,21 +7,45 @@ export default function Navbar(){
 
     return(
 
+        // <nav className="navbar border-bottom shadow-sm">
+
+        //     <div className="container">
+
+        //         <span className="navbar-brand font-bold"> SOLDI Tracker </span>
+        //         <span>
+        //             <NavLink to="/profile"> <i className="bi bi-person-fill"></i> </NavLink>
+        //         </span>
+        //         <span> 
+        //             <ThemeToggle />
+        //         </span>
+            
+        //     </div>
+
+        // </nav>
+
+
         <nav className="navbar border-bottom shadow-sm">
 
-            <div className="container">
+            <div className="container d-flex justify-content-between align-items-center">
 
-                <span className="navbar-brand font-bold"> SOLDI Tracker </span>
-                <span>
-                    <NavLink to="/profile"> <i className="bi bi-person-fill"></i> </NavLink>
+                <span className="navbar-brand mb-0">
+                    SOLDI Tracker
                 </span>
-                <span> 
+
+                <div className="d-flex align-items-center gap-3">
+
+                    <NavLink to="/profile">
+                        <i className="bi bi-person-fill"></i>
+                    </NavLink>
+
                     <ThemeToggle />
-                </span>
-            
+
+                </div>
+
             </div>
 
         </nav>
+
 
     );
 
